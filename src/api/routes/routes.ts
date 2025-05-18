6ac7afbe6893f18f039ae8326d2eaac33931e110
Julián Aleksei Narváez Vinueza <alekseinavalforever@gmail.com>
2025-05-17 21:03:52 -0500

    webhook

--- src/api/routes/routes.ts
+++ src/api/routes/routes.ts
@@ -6,4 +6,10 @@ const router = Router();
 // Ruta para "Hola mundo"
 router.get('/hello', helloService.getHelloWorld);
 
+// Ruta para el webhook de PayPal
+router.post('/paypal-webhook', helloService.handlePayPalWebhook);
+
+// Ruta para fetch y log de ngrok URL
+router.post('/fetch-ngrok', );
+
 export default router;
